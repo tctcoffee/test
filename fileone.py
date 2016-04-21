e6a4476741dc44826c1181922adf73575ccd550a
tctcoffee@163.com <root@localhost.localdomain>
2016-04-21 17:11:45 +0800

    v1.0

---
+++ fileone.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+ 
+# File Name: fileone.py
+# Author: YourName
+# mail: YourEmail
+# Created Time: 2016-04-21
+
+import re
+print "this is a beginning!"
